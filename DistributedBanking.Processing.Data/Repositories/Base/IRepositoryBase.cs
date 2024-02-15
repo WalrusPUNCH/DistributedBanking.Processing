3820470dd5e7f9d20f6d765bf78aef3410dbdc70
vadym.mulish <vadym.mulish@pm.bet>
2024-02-15 22:47:52 +0200

    fix some bugs

--- DistributedBanking.Processing.Data/Repositories/Base/IRepositoryBase.cs
+++ DistributedBanking.Processing.Data/Repositories/Base/IRepositoryBase.cs
@@ -1,6 +1,6 @@
-﻿using System.Linq.Expressions;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using Shared.Data.Entities;
+using System.Linq.Expressions;
 
 namespace DistributedBanking.Processing.Data.Repositories.Base;
 
@@ -8,7 +8,6 @@ public interface IRepositoryBase<T> where T : BaseEntity
 {
     Task AddAsync(T entity);
     Task<IReadOnlyCollection<T>> GetAllAsync();
-    Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T, bool>> filter);
     Task<T?> GetAsync(ObjectId id);
     Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>>? filter = null);
     Task RemoveAsync(ObjectId id);
